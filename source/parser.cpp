279b43fea12f41b9ae874b1bbdb5369324de4d6a
Chigric <ggggg.writer.99@mail.ru>
2018-12-09 21:36:44 +0300

    change lexer

--- source/parser.cpp
+++ source/parser.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-#include "stdio.h"
-#include "ctype.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 #include "parser.h"
 
@@ -11,8 +12,9 @@ Parser::Parser()
 {
 	printStatus = true;
 	lineno = 0;
-	lookahead = -1;
+	charno = 0;
 
+	curToken = new Variable;
 	iStream = nullptr;
 
 	std::cout << "I'am parser" << std::endl;
@@ -22,6 +24,9 @@ Parser::~Parser()
 {
 	if (iStream != nullptr)
 		delete iStream;
+
+	if (curToken != nullptr)
+		delete curToken;
 }
 
 void Parser::parse(const char* fileName)
@@ -29,7 +34,10 @@ void Parser::parse(const char* fileName)
 	iStream = new ifstream;
     iStream->open(fileName, ifstream::in);
 
-    lookahead = this->lexan();
+	if (this->lexan() == NONE) {
+		std::cerr << "ERROR 0x3: some problem with lexan\n";
+		return;
+	}
 	this->printToken();
 
     iStream->close();
@@ -37,41 +45,95 @@ void Parser::parse(const char* fileName)
 
 void Parser::printToken()
 {
-	if (printStatus == true && lookahead != -1)
-		printf("print %d\n", lookahead);
+	if (printStatus == true || curToken->token_id != NONE)
+		std::cout << curToken->lexbuf << std::endl;
 }
 
 const int Parser::lexan()
 {
+	curToken->clear();
+
 	if (iStream == nullptr)
 		return -1;
 
+	char symbol;
 	while (true) {
-		char symbol;
-		iStream->get(symbol);
+		nextChar(symbol);
 
-	    if (isdigit(symbol)) {          //symbol - numeral
-	        // cerr << symbol << endl;
-	    } else if (isalpha(symbol)) {   //symbol - char
-	        // cerr << symbol << endl;
-	    } else switch (symbol) {
-	        case '#':
-				printStatus = false;
-	            break;
-	        case '+':
-	            // std::cerr << '+' << std::endl;
-	            break;
-	        case '\n':
-				if (printStatus == false)
-					printStatus = true;
+		std::cout << "lexan " << symbol << std::endl;
 
-	            std::cerr << "lineno = " << ++lineno << std::endl;
-	            return '\n';
-	        case ' ':
-	        case '\t':
-	        default:
-	            return 0;
-	    }
+		if (symbol == ' ' || symbol == '\t')	//symbol - nothing
+			continue;
+		else if (symbol == '#') {
+			printStatus = false;
+			iStream->unget();
+			getline(static_cast<std::istream&>(*iStream), curToken->lexbuf);
+			std::cout << "\tlexan (comment line) " <<
+			 	curToken->lexbuf << std::endl;
+			curToken->clear();
+
+			continue;
+		} else if (symbol == '\n') {	//symbol - new line
+			if (printStatus == false)
+				printStatus = true;
+			charno = 0;
+	        std::cout << "\tlexan (№ lineno) = " << ++lineno << std::endl;
+
+			continue;
+		} else if (isdigit(symbol)) {	//symbol - numeral
+	        std::cout << "\tlexan (digit) " << symbol << std::endl;
+			curToken->token_id = NUM;
+
+			while (isdigit(symbol)) {
+				curToken->lexbuf.push_back(symbol);
+				nextChar(symbol);
+			}
+			iStream->unget();
+			std::cout << "\tlexan (full number) " <<
+				curToken->lexbuf << std::endl;
+			return 0;
+
+	    } else if (isalpha(symbol) || symbol == '_') {   //symbol - char
+	        std::cout << "\tlexan (alpha) " << symbol << std::endl;
+			curToken->token_id = STRING;
+
+			while (isalnum(symbol)) {
+				curToken->lexbuf.push_back(symbol);
+				nextChar(symbol);
+			}
+			iStream->unget();
+			return 0;
+
+	    } else if (symbol == '<') { 	//symbol - operator '<'
+			std::cout << "\tlexan (LESS) " << symbol << std::endl;
+
+			return LESS;
+		} else if (symbol == '=') {		//symbol = operator '='
+		 nextChar(symbol);
+			if (symbol == '=')			//symbol = operator "=="
+				return NONE;
+			else
+				return -1;
+		} else if (iStream->eof()) {
+			curToken->token_id = NONE;
+			return NONE;
+		} else {
+			curToken->token_id = DONE;
+			return DONE;
+		}
 	}
-    return 0;
+	return -1;
+}
+
+
+void Parser::nextChar(char& _c)
+{
+	if (iStream == nullptr) {
+		std::cerr << "ERROR 0x2: in nextChar():" <<
+			"(" << lineno << ":" << charno << ") " <<
+			"ты совсем ку-ку,\niStream = nullptr\n";
+		return;
+	}
+	iStream->get(_c);
+	++charno;
 }
--- source/parser.h
+++ source/parser.h
@@ -1,22 +1,42 @@
 #pragma once
 
 #include <fstream>
+#include <string>
 
 class Parser
 {
 private:
     //id of symbols
-    enum TYPES {NUM = 256};
+    enum TYPES {NONE = -1, NUM = 256, STRING, LESS, COMPARE, DONE};
 
+    //constants
+    static const char EOS = '\0';
+    // static const int BSIZE = 128;
+
+    struct Variable {
+        int token_id;
+        int value;
+        std::string lexbuf;
+
+        void clear() {
+            token_id = 0;
+            value = 0;
+            lexbuf.clear();
+        }
+    };
+
+    Variable* curToken;
     std::ifstream* iStream;
 
     unsigned int lineno;
+    unsigned int charno;
     int lookahead;
 
     bool printStatus;
 
     void printToken();
     const int lexan();
+    void nextChar(char&);
 public:
     Parser();
     ~Parser();
